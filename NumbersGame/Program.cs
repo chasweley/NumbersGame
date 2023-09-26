b5cb4bcd1d2f6e2651a04a085a92a3d06e43d380
charl <charl@CHARLOTTEAZ>
2023-09-26 20:36:04 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+
+            Random random = new Random();
+            int number = random.Next(1, 20);
+
+            for (int i = 0; i < 5; i++)
+            {
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("Wohoo! Du klarade det!");
+                    break;
+                }
+                else if (guess < number) {
+                    Console.WriteLine("Tyvärr du gissade för lågt!");
+                }
+                else
+                {
+                    Console.WriteLine("Tyvärr du gissade för högt!");
+                }
+            }
+    }
+}
