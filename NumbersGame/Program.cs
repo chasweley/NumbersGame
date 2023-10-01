d85b3296f248e1b436389eb84546af8a45a767bd
Charlotte <charlotteleyser@icloud.com>
2023-10-01 12:39:27 +0200

    Added function CheckIfEqual to check if guess is equal to number. Added if-statments and comments.

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -6,27 +6,40 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer mellan 1 och 20. Kan du gissa vilket? Du får fem försök.");
 
             Random random = new Random();
-            int number = random.Next(1, 20);
+            int number = random.Next(1, 20); //Creates a random number between 1 and 20
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++) //Loop to limit number of guesses
             {
-                int guess = int.Parse(Console.ReadLine());
+                int guess = int.Parse(Console.ReadLine()); //User input for guess
 
-                if (guess == number)
+                if (CheckIfEqual(guess, number)) //If-statement calls function to check if the guess is correct
                 {
                     Console.WriteLine("Wohoo! Du klarade det!");
-                    break;
+                    break; //If method true, break loop
                 }
-                else if (guess < number) {
-                    Console.WriteLine("Tyvärr du gissade för lågt!");
-                }
-                else
+                else if (i < 4 && guess < number) //If iteration lower than four and guess lower than number
                 {
-                    Console.WriteLine("Tyvärr du gissade för högt!");
+                    Console.WriteLine("Tyvärr du gissade för lågt!"); //Print that guess is too low 
                 }
+                else if (i < 4 && guess > number) //If iteration lower than four and guess higher than number
+                {
+                    Console.WriteLine("Tyvärr du gissade för högt!"); //Print that the guess is to high 
+                }
+                if (i == 4 && guess != number) //If interation is equal to four (last iteration allowed) and guess is not equal to number
+                {
+                    Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!"); //Print that guess is incorrect and no more tries
+                }
+
             }
+
+        }
+
+        static bool CheckIfEqual(int guess, int number) //Function to check if guess is equal to number
+        {
+            return guess == number; //Returns true or false
+        }
     }
 }
