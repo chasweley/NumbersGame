dea6bc24e1e31d363ce9ba1fd8f9d25cee8153ce
Charlotte <charlotteleyser@icloud.com>
2023-10-01 19:26:49 +0200

    Refactor code, for-loop exchanged to while-loop, function added to check if guess is too high/low

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -11,35 +11,50 @@ namespace NumbersGame
             Random random = new Random();
             int number = random.Next(1, 20); //Creates a random number between 1 and 20
 
-            for (int i = 0; i < 5; i++) //Loop to limit number of guesses
-            {
-                int guess = int.Parse(Console.ReadLine()); //User input for guess
+            int i = 0;
+            int guess = 0;
 
-                if (CheckIfEqual(guess, number)) //If-statement calls function to check if the guess is correct
+            while (i < 5 && !CheckIfCorrect(guess, number)) //Loop to limit number of guesses and calls function to check if incorrect guess
+            {
+                guess = int.Parse(Console.ReadLine()); //User input for guess
+
+                if (CheckIfCorrect(guess, number)) //If guess is correct
                 {
                     Console.WriteLine("Wohoo! Du klarade det!");
-                    break; //If method true, break loop
                 }
-                else if (guess < number) //If guess lower than number
+
+                else //If guess is incorrect
                 {
-                    Console.WriteLine("Tyvärr du gissade för lågt!"); //Print that guess is too low 
+                    Console.WriteLine($"Tyvärr, du gissade för {HighLow(guess, number)}!"); //Calls function to print string if guess is too low or too high
                 }
-                else if (guess > number) //If guess higher than number
+            
+                if (i == 4 && !CheckIfCorrect(guess, number)) //If-statment for last iteration (i) allowed and guess is incorrect
                 {
-                    Console.WriteLine("Tyvärr du gissade för högt!"); //Print that the guess is too high 
+                    Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!"); //Print that guess is incorrect and no more tries
                 }
-                if (i == 4 && !CheckIfEqual(guess, number)) //If interation is equal to four (last iteration allowed) and guess is not equal to number
-                {
-                    Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!"); //Print that guess is incorrect and no more tries
-                }
+
+                i++;
 
             }
 
         }
 
-        static bool CheckIfEqual(int guess, int number) //Function to check if guess is equal to number
+        static bool CheckIfCorrect(int guess, int number) //Function to check if guess is equal to number
         {
             return guess == number; //Returns true or false
+        }
+
+        static string HighLow(int guess, int number) //Function to return string if the guess is too low or too high
+        {
+            if (guess < number)
+            {
+                return "lågt";
+            }
+
+            else
+            {
+                return "högt";
+            }
         }
     }
 }
